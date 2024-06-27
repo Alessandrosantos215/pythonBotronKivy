57d94c4dc58dca409c4b5e627e0899366b53c891
Alessandrosantos215 <117760649+Alessandrosantos215@users.noreply.github.com>
2024-06-27 07:51:20 -0300

    Initial commit

---
+++ test001 - Copia.py
@@ -0,0 +1,22 @@
+import kivy
+from kivy.app import App
+from kivy.uix.button import Button
+
+class MyApp(App):
+
+    def build(self):
+        # Criar um botão
+        btn = Button(text='Clique-me!',
+                     size_hint=(.5, .5),
+                     pos_hint={'center_x': .5, 'center_y': .5})
+
+        # Associar a função on_press ao evento de clique do botão
+        btn.bind(on_press=self.on_button_click)
+
+        return btn
+
+    def on_button_click(self, instance):
+        print('Botão clicado!')
+
+if __name__ == '__main__':
+    MyApp().run()
